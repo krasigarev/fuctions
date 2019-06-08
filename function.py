65ceeba4f5c7ea379312fa89ca076cb1efdd51c2
Krasimir  Garev <kgarev@gmail.com>
2019-06-08 17:28:03 +0200

    functions

---
+++ function.py
@@ -0,0 +1,11 @@
+num1 = float(input())
+num2 = float(input())
+result = None
+
+
+def get_product(num_1, num_2):
+    result_1 = num_1 * num_2
+    return result_1
+
+
+print(get_product(num1, num2))
