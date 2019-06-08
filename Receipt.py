deb878404c03dabea1e1fd7b0ff8c2dde8a03ba5
Krasimir  Garev <kgarev@gmail.com>
2019-06-08 20:50:42 +0200

    functions

---
+++ Receipt.py
@@ -0,0 +1,18 @@
+def print_header():
+    print("\nCASH RECEIPT\n----------------------\n")
+
+
+def print_body():
+    print("Charged to _______________\nReceived by_______________\n")
+
+
+def print_footer():
+    print("----------------------\n@ Softuni")
+
+
+def print_receipt():
+    print_header()
+    print_body()
+    print_footer()
+
+print_receipt()
