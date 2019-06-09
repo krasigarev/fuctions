548736abb773fcbc3b9e22c237547a1d164b0b74
Krasimir  Garev <kgarev@gmail.com>
2019-06-09 07:52:38 +0200

    functions

--- num.py
+++ num.py
@@ -3,14 +3,14 @@
 n = float(input())
 
 
-def sign(number):
+def sign_of_integer(number):
     if number < 0:
         print('The number ' + str(number) + ' is negative.')
     elif number > 0:
-        print('The number ' + str(number) + ' positive.')
+        print(f'The number  {number}  positive.')
     else:
         print('The number 0 is zero.')
 
 
-sign(n)
+sign_of_integer(n)
 
